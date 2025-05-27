9e8b8a4ee3f3fd2c5abe6d8b437326403d2d388a
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-05-27 17:25:06 -0400

    Initial Commit

---
+++ CalendarSleeper/CalendarSleeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarSleeperApp.swift
+//  CalendarSleeper
+//
+//  Created by Shapiro, Daniel on 5/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CalendarSleeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
