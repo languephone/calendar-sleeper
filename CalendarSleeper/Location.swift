2eb61eb858c707eb024a36a8b408a82f3e422b67
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-05-28 08:05:59 -0400

    Composed single day view.

---
+++ CalendarSleeper/Location.swift
@@ -0,0 +1,16 @@
+//
+//  Location.swift
+//  CalendarSleeper
+//
+//  Created by Shapiro, Daniel on 5/27/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct Location {
+    let name: String
+    let primaryColor: Color
+    let targetDays: Int
+    var currentDays: Int
+}
