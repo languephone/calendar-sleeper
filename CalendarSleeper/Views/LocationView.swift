914bc84f2144dd1c23fcd73274e9a6d1bd7eaaca
Daniel Shapiro <73309519+languephone@users.noreply.github.com>
2025-08-18 22:11:36 -0400

    Add Slider to control targetDays in LocationListView.

--- CalendarSleeper/Views/LocationView.swift
+++ CalendarSleeper/Views/LocationView.swift
@@ -9,16 +9,34 @@ import SwiftUI
 
 struct LocationView: View {
     @Bindable var location: Location
+    @State var tempTargetDays: Double
     
     var body: some View {
         Form {
+            // Edit Location Name
             TextField("Location Name", text: $location.name)
                 .font(.title)
                 .textInputAutocapitalization(.words)
                 .disableAutocorrection(true)
+            
+            // Display accumulated days
             Text("Current Days: \(location.currentDays)")
-            Stepper(value: $location.targetDays, in: 0...365, step: 1) {
-                Text("Target Days: \(location.targetDays)")
+            
+            // Edit Target Days via Slider & Stepper
+            VStack(alignment: .leading) {
+                // Stepper and Slider both work on the tempTargetDays variable,
+                // then move changes to the location.targetDays attribute when
+                // The onEditingChanged value updates.
+                Stepper(value: $tempTargetDays,
+                        in: 0...365,
+                        label: { Text("Target Days: \(Int(tempTargetDays))") },
+                        onEditingChanged: { _ in location.targetDays = Int(tempTargetDays) }
+                )
+                Slider(value: $tempTargetDays,
+                       in: 0...365,
+                       label: {Text("Temp Days")},
+                       onEditingChanged: { _ in  location.targetDays = Int(tempTargetDays) }
+                )
             }
             Section {
                 Button(role: .destructive) {
@@ -32,5 +50,5 @@ struct LocationView: View {
 
 #Preview {
     @Previewable @State var location = Location(name: "London", targetDays: 120)
-    LocationView(location: location)
+    LocationView(location: location, tempTargetDays: Double(location.targetDays))
 }
